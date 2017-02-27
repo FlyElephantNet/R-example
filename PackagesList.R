d3b50fc43e951a25d1aec182020a874b02f50fcf
Dmitry Spodarets <d.spodarets@flyelephant.net>
2017-02-27 13:42:59 +0200

    Update PackagesList.R

--- PackagesList.R
+++ PackagesList.R
@@ -1,5 +1,3 @@
-ip <- as.data.frame(installed.packages()[,c(1,3:4)])
-rownames(ip) <- NULL
-ip <- ip[is.na(ip$Priority),1:2,drop=FALSE]
-print(ip, row.names=FALSE)
-write.csv(ip, file = "~/workdir/MyData.csv")
+packinfo <- installed.packages ()
+print(packinfo, row.names=FALSE)
+write.csv(packinfo, file = "~/workdir/MyData.csv")
